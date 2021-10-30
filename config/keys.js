131649edcc1a07d86f900bf8f811b77a6062f351
kmaltcev <k.maltcev@outlook.com>
2021-10-30 13:32:41 +0300

    finished project

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    secretOrKey: "",
+    FRONTEND_HOST: 'http://localhost:3000',
+
+    FACEBOOK_CLIENT_ID: "",
+    FACEBOOK_CALLBACK_URL: "/auth/facebook/callback",
+    FACEBOOK_CLIENT_SECRET: "",
+
+    GOOGLE_CALLBACK_URL: "/auth/google/callback",
+    GOOGLE_CLIENT_SECRET: "",
+    GOOGLE_CLIENT_ID: "",
+
+    mongoURI: "mongodb+srv://<user>:<password>@kanban-board.yf08w.mongodb.net/Kanban?retryWrites=true&w=majority"
+};
